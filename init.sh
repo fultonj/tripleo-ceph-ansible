0ce9a6d2b7b047f758c8a0baabaf62deb1cdfaa9
John Fulton <fulton@redhat.com>
2017-03-16 22:17:59 +0000

    updating init.sh to set up the prereqs described in 7f589ed0de29fff3590686108107a44f2fc2862a

--- init.sh
+++ init.sh
@@ -1,55 +1,105 @@
 #!/usr/bin/env bash
 
-FORK=1
-HACK=0
-CLONE=0
-RPM=0
+MISTRAL=0
+MISTRAL_FORK=0
+MISTRAL_PRIV=1
+
+CEPH_ANSIBLE=0
+CEPH_ANSIBLE_MASTER=0
+
+HEAT=0
+THT=1
 
 source ~/stackrc
-if [ $FORK -eq 1 ]; then
-    # pull from my work isntead of pip
-    git clone https://github.com/fultonj/mistral-ansible-actions.git
-    sudo rm -Rf /usr/lib/python2.7/site-packages/mistral_ansible*
-    pushd mistral-ansible-actions
-    sudo python setup.py install
-    popd
-else
-    sudo yum install -y python-pip
-    sudo pip install mistral-ansible-actions;
-fi    
-sudo mistral-db-manage populate;
-sudo systemctl restart openstack-mistral*;
-mistral action-list | grep ansible
 
-echo "Try these:"
-echo "  mistral action-get ansible"
-echo "  mistral action-get ansible-playbook"
-
-if [ $HACK -eq 1 ]; then
-    # use this workaround for now
-    id mistral
-    if [ ! -d /home/mistral ]; then
-	sudo mkdir /home/mistral
+if [ $MISTRAL -eq 1 ]; then
+    echo "Installing Mistral Ansbile actions from out of tree"
+    # https://github.com/d0ugal/mistral-ansible-actions
+    if [ $MISTRAL_FORK -eq 1 ]; then
+	# pull from my work isntead of pip
+	git clone https://github.com/fultonj/mistral-ansible-actions.git
+	sudo rm -Rf /usr/lib/python2.7/site-packages/mistral_ansible*
+	pushd mistral-ansible-actions
+	sudo python setup.py install
+	popd
+    else
+	sudo yum install -y python-pip
+	sudo pip install mistral-ansible-actions;
+    fi    
+    sudo mistral-db-manage populate;
+    sudo systemctl restart openstack-mistral*;
+    mistral action-list | grep ansible
+    echo "Try these:"
+    echo "  mistral action-get ansible"
+    echo "  mistral action-get ansible-playbook"
+    
+    if [ $MISTRAL_PRIV -eq 1 ]; then
+	# use this workaround for now
+	id mistral
+	if [ ! -f /etc/sudoers.d/mistral ]; then
+	    # escalate mistral users privs so he can run inventory script (for now)
+	    sudo sh -c "echo \"mistral ALL=(root) NOPASSWD:ALL\" | tee -a /etc/sudoers.d/mistral "
+	    sudo chmod 0440 /etc/sudoers.d/mistral
+	fi
+	# I think this can go, but want to test first
+	if [ ! -d /home/mistral ]; then
+	    sudo mkdir /home/mistral
+	fi
+	sudo chown mistral:mistral /home/mistral/
+	sudo cp -r ~/.ssh/ /home/mistral/
+	sudo chown -R mistral:mistral /home/mistral/.ssh/
     fi
-    sudo chown mistral:mistral /home/mistral/
-    sudo cp -r ~/.ssh/ /home/mistral/
-    sudo chown -R mistral:mistral /home/mistral/.ssh/
+fi
 
-    # disable host key checking
+if [ $CEPH_ANSIBLE -eq 1 ]; then
+    echo "Installing ceph-ansible in /usr/share"
+    if [ $CEPH_ANSIBLE_MASTER -eq 1]; then 
+	git clone git@github.com:ceph/ceph-ansible.git
+	sudo mv ceph-ansible /usr/share/
+    else
+	# The latest ceph-ansible CI RPM builds are listed at
+	# https://shaman.ceph.com/repos/ceph-ansible/master/
+	# https://shaman.ceph.com/api/repos/ceph-ansible/master/latest/centos/7/repo?arch=noarch
+	pushd /etc/yum.repos.d
+	sudo sh -c "curl https://2.chacra.ceph.com/repos/ceph-ansible/master/661a9d0cdf35eb7d4b40ae25eaf4e8caa0e2dd18/centos/7/flavors/default/repo > ceph.repo"
+	popd
+	sudo yum -y install ceph-ansible
+	stat /usr/share/ceph-ansible/site.yml.sample
+    fi
+    
+    echo "Disabling Ansible host key checking"
     # https://github.com/openstack/tripleo-validations/blob/master/ansible.cfg#L3
     sudo sed -i -e s/\#host_key_checking\ =\ False/host_key_checking=False/g /etc/ansible/ansible.cfg
 fi
 
-if [ $CLONE -eq 1 ]; then
-    git clone git@github.com:ceph/ceph-ansible.git
+if [ $HEAT -eq 1 ]; then
+    echo "Installing new Heat Resource from https://review.openstack.org/#/c/420664/"
+    # https://review.openstack.org/#/c/420664/
+    sudo cp heat/workflow_execution.py /usr/lib/python2.7/site-packages/heat/engine/resources/openstack/mistral/
+    sudo systemctl restart openstack-heat-engine
+    openstack orchestration resource type show --template-type hot OS::Mistral::WorkflowExecution
 fi
 
-if [ $RPM -eq 1 ]; then
-    # The latest ceph-ansible CI RPM builds are listed at
-    # https://shaman.ceph.com/repos/ceph-ansible/master/
-    # https://shaman.ceph.com/api/repos/ceph-ansible/master/latest/centos/7/repo?arch=noarch
-    pushd /etc/yum.repos.d
-    curl https://2.chacra.ceph.com/repos/ceph-ansible/master/661a9d0cdf35eb7d4b40ae25eaf4e8caa0e2dd18/centos/7/flavors/default/repo > ceph.repo
+if [ $THT -eq 1 ]; then
+    dir=/home/stack/tripleo-heat-templates
+    if [ ! -d  $dir ]; then
+	# https://github.com/fultonj/oooq/blob/master/setup-deploy-artifacts.sh
+	echo "$dir is missing; please git clone it from review.openstack.org"
+	exit 1
+    fi
+    if [[ $(ssh-add -l | wc -l) -eq 0 ]]; then
+	# did they forward their SSH key?
+	echo "No SSH agent with keys present. Will not be able to connect to git."
+	exit 1
+    fi
+    pushd $dir
+    # download the smaller change
+    git review -d 404499
+    md5sum overcloud.j2.yaml overcloud-resource-registry-puppet.j2.yaml
+    
+    # download in the bigger change
+    git review -d 441137
+    # checksums should remain the same (no file conflicts here)
+    md5sum overcloud.j2.yaml overcloud-resource-registry-puppet.j2.yaml
     popd
-    yum -y install ceph-ansible 
 fi
