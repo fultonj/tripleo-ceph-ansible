204685e0efb65f7c81e0135c554eddfa60018223
John Fulton <fulton@redhat.com>
2017-03-22 17:03:23 +0000

    update see-last-task.sh to get its own UUID

--- see-last-task.sh
+++ see-last-task.sh
@@ -1,6 +1,17 @@
 #!/usr/bin/env bash
 source ~/stackrc
-TASK_ID=$(cat TASK_ID)
-mistral task-list | tail -2 | head -1
-#mistral task-get $TASK_ID
-mistral task-get-result $TASK_ID | jq . | sed -e 's/\\n/\n/g' -e 's/\\"/"/g'
+WORKFLOW='mistral-ceph-ansible'
+UUID=$(mistral execution-list | grep $WORKFLOW | awk {'print $2'} | tail -1)
+
+if [ -z $UUID ]; then
+    echo "Error: unable to find UUID. Exixting."
+    exit 1
+fi
+
+mistral execution-get $UUID
+echo "Getting output for the following tasks in workflow $WORKFLOW"
+mistral task-list $UUID
+for TASK_ID in $(mistral task-list $UUID | awk {'print $2'} | egrep -v 'ID|^$'); do
+    mistral task-get $TASK_ID
+    mistral task-get-result $TASK_ID | jq . | sed -e 's/\\n/\n/g' -e 's/\\"/"/g'
+done
