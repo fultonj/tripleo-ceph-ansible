75d9e21a7c914473d0728d845dd819ead788ca7f
John Fulton <fulton@redhat.com>
2017-03-22 17:20:16 +0000

    update see-last-task.sh to only show last task (though it can figure out the ID on its own

--- see-last-task.sh
+++ see-last-task.sh
@@ -2,16 +2,15 @@
 source ~/stackrc
 WORKFLOW='mistral-ceph-ansible'
 UUID=$(mistral execution-list | grep $WORKFLOW | awk {'print $2'} | tail -1)
-
 if [ -z $UUID ]; then
     echo "Error: unable to find UUID. Exixting."
     exit 1
 fi
+TASK_ID=$(mistral task-list $UUID | awk {'print $2'} | egrep -v 'ID|^$' | tail -1)
+if [ -z $TASK_ID ]; then
+    echo "Error: unable to find TASK_ID. Exixting."
+    exit 1
+fi
+#mistral task-get $TASK_ID
+mistral task-get-result $TASK_ID | jq . | sed -e 's/\\n/\n/g' -e 's/\\"/"/g'
 
-mistral execution-get $UUID
-echo "Getting output for the following tasks in workflow $WORKFLOW"
-mistral task-list $UUID
-for TASK_ID in $(mistral task-list $UUID | awk {'print $2'} | egrep -v 'ID|^$'); do
-    mistral task-get $TASK_ID
-    mistral task-get-result $TASK_ID | jq . | sed -e 's/\\n/\n/g' -e 's/\\"/"/g'
-done
