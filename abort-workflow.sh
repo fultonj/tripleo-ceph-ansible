df19ae31129648dd5e6228738a787c62dea72254
John Fulton <fulton@redhat.com>
2017-05-26 21:11:03 +0000

    Update abort-workflow.sh
    - don't kill ceph processes on clients

--- abort-workflow.sh
+++ abort-workflow.sh
@@ -10,6 +10,6 @@ AEID=$(mistral action-execution-list | grep -i running | tail -1 | awk {'print $
 echo "mistral action-execution-update $AEID --state ERROR" 
 mistral action-execution-update $AEID --state ERROR 
 
-process=ceph
-echo "killing all $process processes on all hosts"
-ansible all -b -m shell -a "for pid in \$(ps axu | grep $process | grep -v grep | awk {'print \$2'}); do echo \$pid; kill \$pid; done"
+#process=ceph
+#echo "killing all $process processes on all hosts"
+#ansible all -b -m shell -a "for pid in \$(ps axu | grep $process | grep -v grep | awk {'print \$2'}); do echo \$pid; kill \$pid; done"
