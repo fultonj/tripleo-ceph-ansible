fc69c84c5b60cb12cb30ffa591298161a9c65c58
John Fulton <fulton@redhat.com>
2017-06-08 00:22:50 +0000

    update see-last-task.sh to grab a list of tasks of the workflow

--- see-last-task.sh
+++ see-last-task.sh
@@ -1,19 +1,18 @@
 #!/usr/bin/env bash
 source ~/stackrc
-#WORKFLOW='tripleo.storage.v1.ceph-install'      # testing from CLI runs? 
-WORKFLOW='tripleo.overcloud.workflowtasks.step2' # testing from Heat runs? 
+WORKFLOW='tripleo.storage.v1.ceph-install'      # testing from CLI runs? 
+#WORKFLOW='tripleo.overcloud.workflowtasks.step2' # testing from Heat runs? 
 UUID=$(mistral execution-list | grep $WORKFLOW | awk {'print $2'} | tail -1)
 if [ -z $UUID ]; then
     echo "Error: unable to find UUID. Exixting."
     exit 1
 fi
-TASK_ID=$(mistral task-list $UUID | awk {'print $2'} | egrep -v 'ID|^$' | tail -1)
-if [ -z $TASK_ID ]; then
-    echo "Error: unable to find TASK_ID. Exixting."
-    exit 1
-fi
-mistral task-get $TASK_ID
-mistral task-get-result $TASK_ID | jq . | sed -e 's/\\n/\n/g' -e 's/\\"/"/g'
+
+for TASK_ID in $(mistral task-list $UUID | egrep "key|ceph_install" | awk {'print $2'} | egrep -v 'ID|^$'); do
+    echo $TASK_ID
+    mistral task-get $TASK_ID
+    mistral task-get-result $TASK_ID | jq . | sed -e 's/\\n/\n/g' -e 's/\\"/"/g'
+done
 
 export UUID
 export TASK_ID
