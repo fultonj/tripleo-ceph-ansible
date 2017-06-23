085314c25e1cf492cf0df4aa9de0ffa0bb54a712
John Fulton <fulton@redhat.com>
2017-06-23 17:21:14 +0000

    update init.sh to pull in extra environment variables to ansible commands (gerrit 475728)

--- init.sh
+++ init.sh
@@ -1,17 +1,17 @@
 #!/usr/bin/env bash 
 
-DNS=0
+DNS=1
 
-IRONIC=0
+IRONIC=1
 
-CEPH_ANSIBLE=0
-CEPH_ANSIBLE_GITHUB=0 # try latest ceph-ansible
+CEPH_ANSIBLE=1
+CEPH_ANSIBLE_GITHUB=1 # try latest ceph-ansible
 GIT_SSH=0
 
 THT=1
 
-WORKBOOK=0
-SKIP_TAGS=0
+WORKBOOK=1
+EXTRA_ENV=1
 
 source ~/stackrc
 
@@ -38,8 +38,8 @@ if [ $CEPH_ANSIBLE -eq 1 ]; then
     if [ $CEPH_ANSIBLE_GITHUB -eq 1 ]; then
 	echo "Cloning ceph-ansible from github"
 	if [ $GIT_SSH -eq 1 ]; then
-	    #git clone git@github.com:ceph/ceph-ansible.git 
-	    git clone git@github.com:fultonj/ceph-ansible.git 
+	    git clone git@github.com:ceph/ceph-ansible.git 
+	    #git clone git@github.com:fultonj/ceph-ansible.git 
 	else
 	    #git clone -b add_openstack_metrics_pool https://github.com/fultonj/ceph-ansible.git
 	    git clone https://github.com/ceph/ceph-ansible.git
@@ -51,12 +51,12 @@ if [ $CEPH_ANSIBLE -eq 1 ]; then
     fi
     stat /usr/share/ceph-ansible/site-docker.yml.sample
 
-    echo "Updating /etc/ansible/ansible.cfg action_plugins=/usr/share/ceph-ansible/plugins"
-    sudo crudini --set /etc/ansible/ansible.cfg defaults action_plugins /usr/share/ceph-ansible/plugins/actions
+    #echo "Updating /etc/ansible/ansible.cfg action_plugins=/usr/share/ceph-ansible/plugins"
+    #sudo crudini --set /etc/ansible/ansible.cfg defaults action_plugins /usr/share/ceph-ansible/plugins/actions
 
-    echo "Disable retry files given permissions issue with /usr/share (for now)"
-    echo "Remove after fix for: https://github.com/ceph/ceph-ansible/issues/1611"
-    sudo crudini --set /etc/ansible/ansible.cfg defaults retry_files_enabled False
+    #echo "Disable retry files given permissions issue with /usr/share (for now)"
+    #echo "Remove after fix for: https://github.com/ceph/ceph-ansible/issues/1611"
+    #sudo crudini --set /etc/ansible/ansible.cfg defaults retry_files_enabled False
 fi
 
 if [ $THT -eq 1 ]; then
@@ -85,20 +85,20 @@ if [ $WORKBOOK -eq 1 ]; then
     echo "- https://review.openstack.org/#/c/469644"
     pushd $dir
 
-    if [ $SKIP_TAGS -eq 1 ]; then
-	git review -d 475952
-	cp tripleo_common/actions/ansible.py ~/ansible.py-475952
+    if [ $EXTRA_ENV -eq 1 ]; then
+	git review -d 475728
+	cp tripleo_common/actions/ansible.py ~/ansible.py-475728
 	git checkout master
     fi
     git review -d 469644
     popd
 
-    if [ $SKIP_TAGS -eq 1 ]; then
+    if [ $EXTRA_ENV -eq 1 ]; then
 	echo "Update new mistral ansible-playbook action to support --skip-tags"
-	sudo diff -u /usr/lib/python2.7/site-packages/tripleo_common/actions/ansible.py  ~/ansible.py-475952
+	sudo diff -u /usr/lib/python2.7/site-packages/tripleo_common/actions/ansible.py  ~/ansible.py-475728
 	sudo rm -Rf /usr/lib/python2.7/site-packages/tripleo_common*
 	pushd $dir
-	cp ~/ansible.py-475952 tripleo_common/actions/ansible.py 
+	cp ~/ansible.py-475728 tripleo_common/actions/ansible.py 
 	sudo python setup.py install
 	sudo cp /usr/share/tripleo-common/sudoers /etc/sudoers.d/tripleo-common
 	sudo systemctl restart openstack-mistral-executor
