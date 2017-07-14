b2647ddb0c509907c70d6d6ccec6ee7c7402d1fa
John Fulton <fulton@redhat.com>
2017-07-14 20:35:15 +0000

    easily choose ceph ansible versions from sig

--- install-ceph-ansible.sh
+++ install-ceph-ansible.sh
@@ -7,7 +7,16 @@
 URL=http://cbs.centos.org/repos
 SUF=x86_64/os
 
-for repo in storage7-ceph-jewel-release storage7-ceph-common-release; do
+# https://cbs.centos.org/koji/taginfo?tagID=737
+#VERSION=candidate
+
+# https://cbs.centos.org/koji/taginfo?tagID=738
+VERSION=testing
+
+# https://cbs.centos.org/koji/taginfo?tagID=739
+#VERSION=release
+
+for repo in storage7-ceph-jewel-$VERSION storage7-ceph-common-$VERSION; do
     echo "Creating $repo.repo"
     sh -c "cat /dev/null > $repo.repo"
     sh -c "echo \"[$repo]\" >> $repo.repo"
