10bbefa78cf07f722d3259e23fb584e6f61c3389
John Fulton <fulton@redhat.com>
2017-07-14 22:35:41 +0000

    skip_tags no longer needed as this merged https://review.openstack.org/#/c/478659

--- deploy.sh
+++ deploy.sh
@@ -4,10 +4,6 @@ source ~/stackrc
 
 WORKBOOK_DEV=1 # workbook updated 
 if [[ $WORKBOOK_DEV -gt 0 ]]; then
-
-    # is skip_tags commented out?
-    grep skip ~/tripleo-common/workbooks/ceph-ansible.yaml
-
     WORKBOOK=/home/stack/tripleo-common/workbooks/ceph-ansible.yaml
     if [[ ! -e $WORKBOOK ]]; then
 	echo "$WORKBOOK does not exist (see init.sh)"
@@ -22,13 +18,15 @@ if [[ $WORKBOOK_DEV -gt 0 ]]; then
 fi
 
 time openstack overcloud deploy --templates ~/templates \
--r ~/tripleo-ceph-ansible/tht/roles_data.yaml \
 -e ~/templates/environments/docker.yaml \
 -e ~/templates/environments/ceph-ansible/ceph-ansible.yaml \
--e ~/templates/environments/ceph-ansible/ceph-mds.yaml \
 -e ~/templates/environments/low-memory-usage.yaml \
 -e ~/templates/environments/disable-telemetry.yaml \
 -e ~/tripleo-ceph-ansible/tht/overcloud-ceph-ansible.yaml
+
+# Set MDS aside for now
+# -r ~/tripleo-ceph-ansible/tht/roles_data.yaml \
+# -e ~/templates/environments/ceph-ansible/ceph-mds.yaml \
 
 # Had http://sprunge.us/dPaH using docker-ha with ceph-ansible
 #-e ~/templates/environments/docker-ha.yaml \
