6902f6fccbcab6487d0e7e65031058b601d2ef53
John Fulton <fulton@redhat.com>
2017-07-16 23:06:50 +0000

    Update ocata deploy scripts
    - relative reference to wipe disk didn't work, putting my own copy in tht
    - rm disable telemetry; didn't work for ocata (ISTR that being the case)
    - update paths to deal with new upgrade/tht directory

--- upgrade/deploy-ocata.sh
+++ upgrade/deploy-ocata.sh
@@ -4,6 +4,5 @@ source ~/stackrc
 
 time openstack overcloud deploy --templates \
 -e /usr/share/openstack-tripleo-heat-templates/environments/low-memory-usage.yaml \
--e /usr/share/openstack-tripleo-heat-templates/environments/disable-telemetry.yaml \
 -e /usr/share/openstack-tripleo-heat-templates/environments/storage-environment.yaml \
--e ~/tripleo-ceph-ansible/upgrade/ocata.yaml
+-e ~/tripleo-ceph-ansible/upgrade/tht/ocata.yaml
