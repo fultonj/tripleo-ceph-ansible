bea41e8f60519f07969ffdb9511a5fae6fe73898
John Fulton <fulton@redhat.com>
2017-08-10 17:24:42 -0400

    deploy without mds

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@
 
 source ~/stackrc
 
-WORKBOOK_DEV=1 # workbook updated 
+WORKBOOK_DEV=0 # workbook updated 
 if [[ $WORKBOOK_DEV -gt 0 ]]; then
     WORKBOOK=/home/stack/tripleo-common/workbooks/ceph-ansible.yaml
     if [[ ! -e $WORKBOOK ]]; then
@@ -18,16 +18,17 @@ if [[ $WORKBOOK_DEV -gt 0 ]]; then
 fi
 
 time openstack overcloud deploy --templates ~/templates \
--r ~/tripleo-ceph-ansible/tht/roles_data.yaml \
 -e ~/templates/environments/docker.yaml \
 -e ~/templates/environments/ceph-ansible/ceph-ansible.yaml \
--e ~/templates/environments/ceph-ansible/ceph-mds.yaml \
 -e ~/templates/environments/low-memory-usage.yaml \
 -e ~/templates/environments/disable-telemetry.yaml \
 -e ~/templates/environments/docker-centos-tripleoupstream.yaml \
 -e ~/tripleo-ceph-ansible/tht/overcloud-ceph-ansible.yaml
 
 
+# set mds aside for now
+#-e ~/templates/environments/ceph-ansible/ceph-mds.yaml \
+#-r ~/tripleo-ceph-ansible/tht/roles_data.yaml \
 
 
 # Had http://sprunge.us/dPaH using docker-ha with ceph-ansible
