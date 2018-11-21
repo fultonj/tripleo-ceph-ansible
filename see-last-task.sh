bb4d565c8d6a5bc7f7e2925dce80029bf3ac42fe
John Fulton <fulton@redhat.com>
2018-11-21 17:17:45 +0000

    add --limit=-1 to see-last-task

--- see-last-task.sh
+++ see-last-task.sh
@@ -1,16 +1,16 @@
 #!/usr/bin/env bash
 source ~/stackrc
 WORKFLOW='tripleo.storage.v1.ceph-install'
-UUID=$(mistral execution-list | grep $WORKFLOW | awk {'print $2'} | tail -1)
+UUID=$(mistral execution-list --limit=-1 | grep $WORKFLOW | awk {'print $2'} | tail -1)
 if [ -z $UUID ]; then
     echo "Error: unable to find UUID. Exixting."
     exit 1
 fi
 
-for TASK_ID in $(mistral task-list $UUID | grep ceph_install | awk {'print $2'} | egrep -v 'ID|^$'); do
+for TASK_ID in $(mistral task-list $UUID | awk {'print $2'} | egrep -v 'ID|^$'); do
     echo $TASK_ID
-    #mistral task-get $TASK_ID
-    mistral task-get-result $TASK_ID | jq . | sed -e 's/\\n/\n/g' -e 's/\\"/"/g'
+    mistral task-get $TASK_ID
+    #mistral task-get-result $TASK_ID | jq . | sed -e 's/\\n/\n/g' -e 's/\\"/"/g'
 done
 
 export UUID
